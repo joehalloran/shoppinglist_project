93ac3be3fd90737f629102ccbdb240fecf336ef7
joehalloran <joe_halloran@hotmail.co.uk>
2017-02-10 22:01:00 +0000

    Updating views and JS

--- shoppinglist/lists/tests_views.py
+++ shoppinglist/lists/tests_views.py
@@ -9,9 +9,10 @@ class ListsViewsTest(TestCase):
 		"""
 		Setup script: Create some example lists and items to test views.
 		"""
+
 		List.objects.create(
 			name="First List", 
-			owner="Joe Bloggs")
+			owner="joe@example.com")
 		p = List.objects.get(name="First List")
 		for i in range(10):
 			Item.objects.create(
@@ -19,7 +20,7 @@ class ListsViewsTest(TestCase):
 				parentList=p)
 		List.objects.create(
 			name="Second List", 
-			owner="Jane Bloggs")
+			owner="jane@example.com")
 		q = List.objects.get(name="Second List")
 		for i in range(10,20):
 			Item.objects.create(
@@ -29,38 +30,14 @@ class ListsViewsTest(TestCase):
 	def test_mylists_view(self):
 		"""
 		Test mylists view returns lists.
-		TODO: Change test to include OWNERSHIP
 		"""
 		url = reverse('lists:mylists')
 		response = self.client.get(url)
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "First List")
-
-	def test_mylists_detail_view(self):
-		"""
-		Test mylists detail view returns list and list items.
-		"""
-		l= List.objects.get(name="First List")
-		url = reverse('lists:detail', args=[l.id])
-		response = self.client.get(url)
-		self.assertEqual(response.status_code, 200)
-		self.assertContains(response, "First List")
-		self.assertContains(response, "Item 0")
-		self.assertContains(response, "Item 9")
-
-	def test_mylists_detail_view_does_not_include_other_lists_items(self):
-		"""
-		Test mylists detail does not include items from other lists.
-		"""
-		l= List.objects.get(name="First List")
-		url = reverse('lists:detail', args=[l.id])
-		response = self.client.get(url)
-		self.assertEqual(response.status_code, 200)
 		self.assertNotContains(response, "Second List")
-		self.assertNotContains(response, "Item 10")
-		self.assertNotContains(response, "Item 19")
 
-	def test_mylists_edit_view(self):
+	def test_lists_edit_view(self):
 		"""
 		Test mylists edit view returns list, list items, and a save but.
 		"""
@@ -73,7 +50,7 @@ class ListsViewsTest(TestCase):
 		self.assertContains(response, "Item 9")
 		self.assertContains(response, "Save")
 
-	def test_mylists_edit_view_does_not_contain_items_from_other_lists(self):
+	def test_lists_edit_view_does_not_contain_items_from_other_lists(self):
 		"""
 		Test mylist edit view does include items in other lists.
 		"""
--- shoppinglist/lists/views.py
+++ shoppinglist/lists/views.py
@@ -8,9 +8,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.core.exceptions import PermissionDenied
-from django.contrib import messages
-
-from django.forms.formsets import DELETION_FIELD_NAME
+from django.db import transaction
 
 from oauth2client.contrib.django_util import decorators
 from oauth2client import client
@@ -29,46 +27,65 @@ class MyListView(ListView):
 
     @method_decorator(decorators.oauth_required())
     def dispatch(self,request,*args,**kwargs):
+    	"""
+    	Add oauth decorator to bind google user data to request object
+    	"""
         return super(MyListView,self).dispatch(request,*args,**kwargs)
 
     def get_queryset(self):
+    	"""
+    	Only list Lists I own.
+    	"""
         return List.objects.filter(owner = self.request.oauth.credentials.id_token['email'])
 
 class ListCreate(SuccessMessageMixin, CreateView):
+	"""
+	Form to create a new list
+	"""
 	template_name = 'lists/create_form.html'
 	form_class = ListCreateForm
 	success_message = "List Created. Now you can add some items."
 
 	@method_decorator(decorators.oauth_required())
 	def dispatch(self,request,*args,**kwargs):
+		"""
+		Add oauth decorator to bind google user data to request object
+		"""
 		return super(ListCreate,self).dispatch(request,*args,**kwargs)
 
 	def form_valid(self, form):
+		"""
+		Manually add google user as owner
+		"""
 		form.instance.owner = self.request.oauth.credentials.id_token['email']
 		return super(ListCreate, self).form_valid(form)
 
 class ListDelete(DeleteView):
+	"""
+	Form to delete a list.
+	"""
 	model = List
 	success_url = reverse_lazy('lists:mylists')
 	success_message = "Your list deleted successfully."
 
 	@method_decorator(decorators.oauth_required())
 	def dispatch(self,request,*args,**kwargs):
+		"""
+		Add oauth decorator to bind google user data to request object and check if user owns object before delete.
+		"""
 		current_user = request.oauth.credentials.id_token['email']
 		parentList = self.get_object()
 		if current_user != parentList.owner and not request.user.is_superuser: 
 			raise PermissionDenied
-
 		return super(ListDelete,self).dispatch(request,*args,**kwargs)
 
 	def get_context_data(self, **kwargs):
-		# Call the base implementation first to get a context
+		"""
+		Add in a QuerySet of of the items belonging to the list
+		"""
 		context = super(ListDelete, self).get_context_data(**kwargs)
-		# Add in a QuerySet of all the books
 		context['items'] = Item.objects.filter( parentList = self.get_object() )
 		return context
-
-	## TODO: CHECK OWNERSHIP
 
 	def delete(self, request, *args, **kwargs):
 		"""
@@ -80,9 +97,10 @@ class ListDelete(DeleteView):
 @decorators.oauth_required
 def editListView(request, pk):
 	"""
-	Create / process form to edit all items in list.
+	Create / edit form to edit all items in list and list title.
 	"""
-	# Get current user and list object to check ownship
+
+	# Get current user and list object to check ownership
 	current_user = request.oauth.credentials.id_token['email']
 	parentList = List.objects.get(pk=pk)
 	if current_user != parentList.owner and not request.user.is_superuser: 
@@ -98,6 +116,7 @@ def editListView(request, pk):
     		}
 		)
 
+	# If no items exist create an extra field to add new list item.
 	itemCount = Item.objects.filter(parentList = parentList).count()
 	if itemCount > 0:
 		extraItemField = 0
@@ -109,24 +128,30 @@ def editListView(request, pk):
 		List, 
 		Item, 
 		fields=("name", "parentList"), 
-		extra=extraItemField, # Extra items will be added through JS
+		extra=extraItemField, 
 		widgets={
     		'name': TextInput(attrs={'class': 'form-control form-inactive', 'required': True}),
     		}
     	)
+
 	# if POST generate form data and save if valid
 	if request.method == 'POST':
 		item_formset = ItemInlineFormSet( request.POST, instance=parentList )
 		list_formset = ListFormSet( request.POST, queryset= List.objects.filter(pk=pk) )
 		if item_formset.is_valid() and list_formset.is_valid():
-			### TODO: TRANSACTIONS
-			item_formset.save()
-			list_formset.save()
+			# Transactions - only save list and items together
+			with transaction.atomic():
+				item_formset.save()
+				list_formset.save()    
 			# Redirect to list page.
+			messages.add_message(request, messages.SUCCESS, 'List changes saved.')
 			return HttpResponseRedirect(reverse('lists:edit', kwargs={'pk':pk}))
 		else:
-			## TODO Handle error by empty fields
-			messages.error(request, formset.errors)
+			# Add errors and redirect to form.
+			for errors in list_formset.errors:
+				messages.add_message(request, messages.INFO, errors, extra_tags='danger')
+			for errors in item_formset.errors:
+				messages.add_message(request, messages.INFO, errors, extra_tags='danger')
 			return HttpResponseRedirect(reverse('lists:edit', kwargs={'pk':pk}))
 	# if a GET (or any other method) create a blank form
 	else:
@@ -137,8 +162,7 @@ def editListView(request, pk):
 		return render(request, 'lists/item_form.html', {
 			'item_formset': item_formset, 
 			'list_formset': list_formset, 
-			'mylist': parentList}) # Is parentList necessary (if all the work is done by the form )
-
+			}) 
 
 
             
